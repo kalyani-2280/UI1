275de5bdd7401d9765246e8b74c2e0c6fcdb6ae2
kalyani-2280 <kalyanideore553@gmail.com>
2025-06-13 13:59:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css'; // Import the main CSS file for component styles
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
